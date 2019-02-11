c4bc16153e1f3585c3387b4c2368848ff3896d4b
Khiem Le <khiem.le@Khiem-Les-MBP15.local>
2019-02-11 15:39:06 +0700

    Change to use stream replace for setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -46,15 +47,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  StreamController<int> _counterStream = StreamController<int>();
+
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _counterStream.sink.add(++_counter);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _counterStream.close();
   }
 
   @override
@@ -94,10 +96,15 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            new StreamBuilder(
+                stream: _counterStream.stream,
+                initialData: 0,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return Text(
+                    '${snapshot.data}',
+                    style: Theme.of(context).textTheme.display1,
+                  );
+                }),
           ],
         ),
       ),
